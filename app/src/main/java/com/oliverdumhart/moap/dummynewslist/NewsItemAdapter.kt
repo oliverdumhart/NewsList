6c217fe0d13a2cf1ac221d5063a9485dcd05d016
Oliver Dumhart <oliver@dumhart.net>
2020-10-10 16:00:11 +0200

    Add author, date and image to recyclerview item, load image via glide

--- app/src/main/java/com/oliverdumhart/moap/dummynewslist/NewsItemAdapter.kt
+++ app/src/main/java/com/oliverdumhart/moap/dummynewslist/NewsItemAdapter.kt
@@ -3,13 +3,17 @@ package com.oliverdumhart.moap.dummynewslist
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
 import com.oliverdumhart.moap.dummynewslist.NewsItemAdapter.NewsItemViewHolder
 import com.oliverdumhart.moap.dummynewslist.entities.NewsItem
+import com.oliverdumhart.moap.dummynewslist.extensions.toString
 
 class NewsItemAdapter(private val clickListener: NewsItemClickListener, private var items: List<NewsItem> = listOf()) : RecyclerView.Adapter<NewsItemViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsItemViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.news_item, parent, false)
@@ -31,9 +35,17 @@ class NewsItemAdapter(private val clickListener: NewsItemClickListener, private 
 
     inner class NewsItemViewHolder(itemView: View) : ViewHolder(itemView), View.OnClickListener {
         private val title: TextView
+        private val image: ImageView
+        private val author: TextView
+        private val date: TextView
 
         fun bind(item: NewsItem) {
             title.text = item.title
+            author.text = item.author
+            date.text = item.publicationDate?.toString("MMM dd, yyyy, HH:mm") ?: ""
+            item.image?.let {
+                Glide.with(image.context).load(it).into(image)
+            }
         }
 
         override fun onClick(view: View) {
@@ -43,6 +55,9 @@ class NewsItemAdapter(private val clickListener: NewsItemClickListener, private 
 
         init {
             title = itemView.findViewById(R.id.title)
+            image = itemView.findViewById(R.id.image)
+            author = itemView.findViewById(R.id.author)
+            date = itemView.findViewById(R.id.date)
             itemView.setOnClickListener(this)
         }
     }
